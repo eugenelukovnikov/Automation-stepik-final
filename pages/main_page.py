95bc0ae834ec319c435089a6ef8667babf148663
eugene_lukovnikov <Virus3625css@yandex.ru>
2025-02-04 22:49:17 +0400

    В класс BasePage добавлен метод is_element_present для перехватывания исключений.
    Метод перехватывает исключение
    NoSuchElementException и возвращает False, если элемент не найден.

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,15 @@
 from .base_page import BasePage
 
 class MainPage(BasePage): 
+    
+    MAIN_PAGE_URL = "http://selenium1py.pythonanywhere.com/"
+
+    def __init__(self, driver, url=MAIN_PAGE_URL):
+        super().__init__(driver, url)
+
     def go_to_login_page(self):
         login_link = self.driver.find_element('xpath', "//a[@id='login_link']")
         login_link.click()
+    
+    def should_be_login_link(self):
+        assert self.is_element_present('xpath', "//a[@id='login_link']"), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,13 @@ from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(driver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(driver, link)
+    #link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(driver)
     page.open()
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(driver):
+    #link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(driver)
+    page.open()
+    page.should_be_login_link()
